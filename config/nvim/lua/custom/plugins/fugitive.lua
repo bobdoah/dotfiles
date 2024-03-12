4dc1460d72d847a660b652e96893d4aad610e313
Robert Williams <1266467+bobdoah@users.noreply.github.com>
2024-03-12 21:02:34 +0000

    feat: add neotree and fugitive plugins

---
+++ config/nvim/lua/custom/plugins/fugitive.lua
@@ -0,0 +1,10 @@
+return {
+  'tpope/vim-fugitive',
+  version = '*',
+  keys = {
+    { '<leader>gs', '<cmd>Git<cr>', desc = 'Git Status' },
+    { '<leader>gp', '<cmd>Git push<cr>', desc = 'Git Push' },
+    { '<leader>gfp', '<cmd>Git push --force', desc = 'Git Force Push' },
+    { '<leader>gmp', '<cmd>Git push -o merge_request.create=true<cr>', desc = 'Git Push and open Merge Request' },
+  },
+}
