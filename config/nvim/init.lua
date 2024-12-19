707ccb4939cb44cd563a05e4a8c2cf4178507225
Rob Williams <rowillia@akamai.com>
2024-12-19 17:12:17 +0000

    fix: make warnings go away

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -605,6 +605,8 @@ require('lazy').setup({
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
+        ensure_installed = {},
+        automatic_installation = false,
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
