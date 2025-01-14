1b8da7756ccc9fed918582206763edffad9a7a4e
Rob Williams <rowillia@akamai.com>
2025-01-14 17:59:31 +0000

    fix: add missing return

--- config/nvim/lua/custom/plugins/fugitive.lua
+++ config/nvim/lua/custom/plugins/fugitive.lua
@@ -4,7 +4,7 @@ return {
   keys = {
     { '<leader>gs', '<cmd>Git<cr>', desc = 'Git Status' },
     { '<leader>gp', '<cmd>Git push<cr>', desc = 'Git Push' },
-    { '<leader>gfp', '<cmd>Git push --force', desc = 'Git Force Push' },
+    { '<leader>gfp', '<cmd>Git push --force<cr>', desc = 'Git Force Push' },
     { '<leader>gmp', '<cmd>Git push -o merge_request.create=true<cr>', desc = 'Git Push and open Merge Request' },
   },
 }
