f65ed8b7bcc19d321791f277ff71f6b3bbbd7804
Rob Williams <rowillia@akamai.com>
2025-05-15 12:03:20 +0100

    chore: add some more useful git keymaps

--- config/nvim/lua/custom/plugins/fugitive.lua
+++ config/nvim/lua/custom/plugins/fugitive.lua
@@ -5,6 +5,8 @@ return {
   keys = {
     { '<leader>gs', '<cmd>Git<cr>', desc = 'Git Status' },
     { '<leader>gp', '<cmd>Git push<cr>', desc = 'Git Push' },
+    { '<leader>go', '<cmd>Git push -u origin<cr>', desc = 'Git push origin' },
+    { '<leader>gb', '<cmd>Git blame<cr>', desc = 'Git blame' },
     { '<leader>gfp', '<cmd>Git push --force<cr>', desc = 'Git Force Push' },
     { '<leader>gmp', '<cmd>Git push -o merge_request.create=true<cr>', desc = 'Git Push and open Merge Request' },
   },
