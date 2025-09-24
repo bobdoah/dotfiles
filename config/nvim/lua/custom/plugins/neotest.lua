d8af7c829e4bb6d18736bb0a124d39fa0c9743c6
Rob Williams <rowillia@akamai.com>
2025-09-24 16:07:33 +0100

    feat: golang plugin for neotest

--- config/nvim/lua/custom/plugins/neotest.lua
+++ config/nvim/lua/custom/plugins/neotest.lua
@@ -1,9 +1,25 @@
 return {
-  'nvim-neotest/neotest',
-  dependencies = {
-    'nvim-neotest/nvim-nio',
-    'nvim-lua/plenary.nvim',
-    'antoinemadec/FixCursorHold.nvim',
-    'nvim-treesitter/nvim-treesitter',
+  {
+    'nvim-neotest/neotest',
+    dependencies = {
+      'nvim-neotest/nvim-nio',
+      'nvim-lua/plenary.nvim',
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      {
+        'fredrikaverpil/neotest-golang',
+        version = '*',
+      },
+    },
+    config = function()
+      local config = {
+        runner = 'gotestsum', -- Optional, but recommended
+      }
+      require('neotest').setup {
+        adapters = {
+          require 'neotest-golang'(config),
+        },
+      }
+    end,
   },
 }
