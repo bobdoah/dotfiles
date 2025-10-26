e154b4b642469806ebd656a201114716410ccc26
Robert Williams <1266467+bobdoah@users.noreply.github.com>
2025-10-26 21:22:45 +0000

    fix: lsp loading changed

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -600,17 +600,10 @@ require('lazy').setup({
           },
         },
       }
-      for server_name, server_config in pairs(servers) do
-        server_config.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server_config.capabilities or {})
-        vim.lsp.config(server_name, server_config)
-        vim.lsp.enable(server_name)
-      end
 
-      -- You can add other tools here that you want Mason to install
-      -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format lua code
+        'stylua',
         'ruff',
         'shfmt',
         'hclfmt',
@@ -618,8 +611,24 @@ require('lazy').setup({
         'golangci-lint',
         'hadolint',
         'shellcheck',
+        'rustfmt',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
+      require('mason-lspconfig').setup {
+        ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
+        automatic_installation = false,
+        handlers = {
+          function(server_name)
+            local server = servers[server_name] or {}
+            -- This handles overriding only values explicitly passed
+            -- by the server configuration above. Useful when disabling
+            -- certain features of an LSP (for example, turning off formatting for ts_ls)
+            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+            require('lspconfig')[server_name].setup(server)
+          end,
+        },
+      }
     end,
   },
 
